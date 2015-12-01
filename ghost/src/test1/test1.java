419846956ad68c8d0a110ccf6de9c2edc96f4d7e
Kuei <Kuei@192.168.0.10>
2015-12-01 23:14:10 +0800

    second commit

--- ghost/src/test1/test1.java
+++ ghost/src/test1/test1.java
@@ -4,6 +4,11 @@ public class test1 {
 
 	public static void main(String[] args) {
 		System.out.println("123");
+		System.out.println("hahaha");
+		System.out.println("yayaya");
+		System.out.println("ggggggg");
+		System.out.println("nononono");
+		System.out.println("QQQQQQQQQ");
 
 	}
 
